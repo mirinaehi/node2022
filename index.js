fb57e238f1b8aad4bbc1748259025a3111c9a045
shyoon63 <shyoon63@e-mirim.hs.kr>
2022-04-04 14:29:23 +0900

    web2-26 함수화(templateList)

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const http = require('http')
 const fs = require('fs')
 const url = require('url')
 
+function templateList(filelist){
+  let list = '<ul>';
+  for(let i=0; i<filelist.length; i++) {
+    list += `<li> <a href="/?id=${filelist[i]}"> ${filelist[i]} </a> </li>`;
+  }
+  list += '</ul>';
+  return list;
+}
+
 function templateHTML(title, list, body){
   return `
           <!doctype html>
@@ -29,25 +38,16 @@ const app = http.createServer(function (request, response) {
       const title = 'Welcome'
       const description = 'Hello, Node.js'
       fs.readdir('data/', function (err, data){
-        let list = '<ul>';
-        for(let i=0; i<data.length; i++) {
-          list += `<li> <a href="/?id=${data[i]}"> ${data[i]} </a> </li>`;
-        }
-        list += '</ul>';
+        const list = templateList(data);
         const template = templateHTML(title, list, description);
         response.writeHead(200)
         response.end(template)
       })
     } else {
       fs.readdir('data/', function (err, data){
-        let list = '<ul>';
-        for(let i=0; i<data.length; i++) {
-          list += `<li> <a href="/?id=${data[i]}"> ${data[i]} </a> </li>`;
-        }
-        list += '</ul>';
-
-        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-          const title = queryData.id
+          fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          const title = queryData.id;
+          const list = templateList(data);
           const template = templateHTML(title, list, description);
           response.writeHead(200)
           response.end(template)
