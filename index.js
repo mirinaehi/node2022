6ecd450daae75c3861c5a7982d7dea179920540a
newJWP <garook@e-mirim.hs.kr>
2022-05-29 20:45:51 +0900

    web3-9 페이지 수정,삭제 구현

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ app.get('/page/:pageId', function (req, res) {
     fs.readdir('./data', function(err, filelist){
         const list = template.List(filelist);
         const id = req.params.pageId;
-        fs.readFile(`./data/${id}`, 'utf8', function (err, description){
+        fs.readFile(`./data/${id}`, 'utf8', function (error, description){
             const title = id;
             const html = template.HTML(title, list, description
             , `<a href="/create">create</a>
-                <a href="/update?id=${title}">update</a> 
-                <form action="delete_process" method="post">
+                <a href="/update/${title}">update</a> 
+                <form action="/delete_process" method="post">
                     <input type="hidden" name="id" value="${title}">
                     <input type="submit" value="delete">
                 </form>`);
@@ -66,8 +66,65 @@ app.post('/create_process', function(req, res){
     const title = post.title;
     const description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
-      res.writeHead(302, { Location: `/page/${title}` });
-      res.end();
+      res.redirect(`/page/${title}`);
+    });
+  });
+});
+
+app.get('/update/:pageId', function(req, res){
+  fs.readdir('./data', function (err, filelist) {
+    const id = req.params.pageId;
+    fs.readFile(`data/${id}`, 'utf8', function (error, description) {
+      const title = id;
+      const list = template.List(filelist);
+      const html = template.HTML(title, list,
+        `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>`,
+      );
+      res.send(html);
+    });
+  });
+});
+
+app.post('/update_process', function (req, res) {
+  let body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    const post = qs.parse(body);
+    const id = post.id;
+    const title = post.title;
+    const description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        res.redirect(`/page/${title}`);
+      });
+    });
+  });
+});
+
+app.post('/delete_process', function(req, res){
+  let body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    const post = qs.parse(body);
+    const id = post.id;
+    fs.unlink(`data/${id}`, function (error) {
+      res.redirect(`/`);
     });
   });
 });
